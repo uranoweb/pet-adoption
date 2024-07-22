f04a8988ec47533ac3e475f83d52680eaf7431e3
Mutsumi <>
2024-07-22 01:07:32 -0500

    Work in progress, fetched pet data

--- main.js
+++ main.js
@@ -9,3 +9,12 @@ async function start() {
 
 start()
 
+async function petsArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
+  petsData.forEach(pet => {
+    console.log(pet.name)
+  })
+}
+
+petsArea()
